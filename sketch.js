3d6759faf4c9182513b679b419d663445fee02e4
Francesco <francesco.invidia.98@gmail.com>
2019-09-24 21:19:13 +0200

    Cover and sketch

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ function preload(){
 function setup() {
   createCanvas(windowWidth,windowHeight);
   angleMode(DEGREES);
-  frameRate(30);
-  background("aquamarine");
+  frameRate(360);
+  background(150,200,230);
 }
 
 function draw() {
@@ -15,24 +15,39 @@ function draw() {
   stroke(lerpColor(color("red"),color("blue"),frameCount/120));
   strokeWeight(5);
 
-  if (frameCount>=0) {
+  if (0<frameCount && frameCount<=120) {
     var s=0;
     var i=300;
     var d=300;
+    var c=cos(frameCount*3)*d
+    var e=sin(frameCount*3)*d
+    line(i, -s, c, -e);
+    line(-i, s, -c, e);
+
   }
 
-  if (frameCount>=120) {
-      i=1;
-      s=300;
-      strokeWeight(1);
-      stroke("white")
+  if (120<frameCount && frameCount<=240) {
+    push();
+    stroke("red");
+    rotate(90)
+    line(200, 0, cos(frameCount*4)*160, -sin(frameCount*3)*210);
+
   }
 
-  if (frameCount==180) {
-      noLoop()
+
+  if (240<frameCount && frameCount<=360) {
+    push();
+    stroke("black");
+    rotate(90)
+    line(200, 0, cos((frameCount-120)*4)*160, -sin((frameCount-120)*3)*210);
   }
 
-  line(i, -s, cos(frameCount*3)*d, -sin(frameCount*3)*d);
-  line(-i, s, -cos(frameCount*3)*d, sin(frameCount*3)*d);
+  if (360<frameCount && frameCount<=1080) {
+    push();
+    stroke("black")
+    fill(255, 255, 255, 0)
+    ellipse(0,0,3*frameCount-360);
+  }
+
 
 }
